00deba832f71fde93f3a61c0b7d339144ae5218c
yangleilei <oddboy1990@gmail.com>
2016-09-26 14:40:33 +0800

    add lesson 5

---
+++ webpack/lesson5/main.js
@@ -0,0 +1,11 @@
+var smallImg = document.createElement("img")
+smallImg.src = require('./img/small.png')
+document.body.appendChild(smallImg)
+
+var tt = document.createElement("div");
+tt.innerHTML = "<br/>";
+document.body.appendChild(tt);
+
+var bigImg = document.createElement("img")
+bigImg.src = require('./img/big.png')
+document.body.appendChild(bigImg)
